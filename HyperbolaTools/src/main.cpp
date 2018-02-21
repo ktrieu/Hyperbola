b0199f1daf7ba3f504d837de21596bf3ea10cb06
Kevin Trieu <dragn194@gmail.com>
2018-02-21 08:40:49 -0500

    Add arguments to the tools

--- HyperbolaTools/src/main.cpp
+++ HyperbolaTools/src/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <filesystem>
+
+namespace fs = std::experimental::filesystem;
 
 int main(int argc, char** argv) {
-	std::cout << "This is the tools.\n";
-	std::cin.ignore();
+	if (argc < 3) {
+		std::cout << "Usage: HyperbolaTools.exe [resource folder] [output path]\n";
+		return 1;
+	}
+	fs::path resourcePath(argv[1]);
+	fs::path outputPath(argv[2]);
+	std::cout << "== HyperbolaTools v1 ==\n";
+	std::cout << "Resource folder is: " << resourcePath << "\n";
+	std::cout << "Output folder is: " << outputPath << "\n";
 	return 0;
 }
