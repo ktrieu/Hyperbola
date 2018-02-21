e15dc8a4cb648a33f272724794279e85524bba9d
Kevin Trieu <dragn194@gmail.com>
2018-02-21 08:32:38 -0500

    Add the engine and tools projects
    Time to do this again because CMake is cancer.

---
+++ HyperbolaEngine/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+	std::cout << "This is the engine.\n";
+	std::cin.ignore();
+	return 0;
+}
---
+++ HyperbolaTools/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+	std::cout << "This is the tools.\n";
+	std::cin.ignore();
+	return 0;
+}
