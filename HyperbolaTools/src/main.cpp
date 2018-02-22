bf645fdc23e383de2f6dac0ba8e782029ec290d6
Kevin Trieu <dragn194@gmail.com>
2018-02-22 12:20:13 -0500

    Add resource copying

--- HyperbolaTools/src/main.cpp
+++ HyperbolaTools/src/main.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
 #include <filesystem>
+#include <map>
 
 namespace fs = std::experimental::filesystem;
+
+fs::path getPathRelativeTo(fs::path path, fs::path root) {
+	auto pathIter = path.begin();
+	auto rootIter = root.begin();
+	//loop until we find the first different component
+	while (*pathIter == *rootIter) {
+		pathIter++;
+		rootIter++;
+	}
+	fs::path relPath;
+	while (pathIter != path.end()) {
+		relPath.append(*pathIter);
+		pathIter++;
+	}
+	return relPath;
+}
+
+std::map<fs::path, fs::path> resources;
+
+void enumerateResources(fs::path resourcePath, fs::path outputPath) {
+	fs::recursive_directory_iterator resourceIter(resourcePath);
+	for (fs::path path : resourceIter) {
+		//only process files
+		if (fs::is_regular_file(path)) {
+			fs::path relPath = getPathRelativeTo(path, resourcePath);
+			fs::path resourceOutPath = outputPath / relPath;
+			resources[path] = resourceOutPath;
+		}
+	}
+}
+
+void copyResources() {
+	for (auto resource : resources) {
+		//create any subdirectories if they don't exist
+		if (!fs::exists(resource.second.parent_path())) {
+			fs::create_directories(resource.second.parent_path());
+		}
+		fs::copy_file(resource.first, resource.second, fs::copy_options::overwrite_existing);
+	}
+}
 
 int main(int argc, char** argv) {
 	if (argc < 3) {
@@ -13,5 +54,7 @@ int main(int argc, char** argv) {
 	std::cout << "== HyperbolaTools v1 ==\n";
 	std::cout << "Resource folder is: " << resourcePath << "\n";
 	std::cout << "Output folder is: " << outputPath << "\n";
+	enumerateResources(resourcePath, outputPath);
+	copyResources();
 	return 0;
 }
