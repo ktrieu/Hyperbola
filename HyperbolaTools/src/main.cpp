5444bc5705dc1e498e03fbdc4f22299b0d266848
Kevin <dragn194@gmail.com>
2018-03-05 16:57:54 -0500

    Change the tools project to handle paths with spaces

--- HyperbolaTools/src/main.cpp
+++ HyperbolaTools/src/main.cpp
@@ -2,6 +2,7 @@
 #include <filesystem>
 #include <map>
 #include <fstream>
+#include <string>
 
 #include <cereal\cereal.hpp>
 #include <cereal\archives\binary.hpp>
@@ -105,13 +106,21 @@ void processResources() {
 	}
 }
 
+void removeQuotes(std::string& str) {
+	str.erase(std::remove(str.begin(), str.end(), '\"'), str.end());
+}
+
 int main(int argc, char** argv) {
 	if (argc < 3) {
 		std::cout << "Usage: HyperbolaTools.exe [resource folder] [output path]\n";
 		return 1;
 	}
-	fs::path resourcePath(argv[1]);
-	fs::path outputPath(argv[2]);
+	std::string resourcePathStr(argv[1]);
+	removeQuotes(resourcePathStr);
+	fs::path resourcePath(resourcePathStr);
+	std::string outputPathStr(argv[2]);
+	removeQuotes(outputPathStr);
+	fs::path outputPath(outputPathStr);
 	std::cout << "== HyperbolaTools v1 ==\n";
 	std::cout << "Resource folder is: " << resourcePath << "\n";
 	std::cout << "Output folder is: " << outputPath << "\n";
